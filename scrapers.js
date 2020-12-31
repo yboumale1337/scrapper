f5985c71abd4d6ef7a7cfe6940193a0e6598a42e
yboumale1337 <yboumale@smartleet.com>
2020-12-31 23:16:39 +0100

    edit

--- scrapers.js
+++ scrapers.js
@@ -1,10 +1,9 @@
 const puppeteer = require("puppeteer");
 const csv = require("csv-parser");
 const fs = require("fs");
-
 let count = 0;
 const results = [];
-fs.createReadStream("gmail.csv")
+fs.createReadStream("gmail.csv", { start: 0, end: 900 })
   .pipe(
     csv({
       headers: false,
@@ -12,25 +11,28 @@ fs.createReadStream("gmail.csv")
     })
   )
   .on("data", (data) => {
+    let stringify = JSON.stringify(data)
+    console.log(JSON.parse(stringify))
     results.push(data);
     count++;
   })
-  .on("end", () => {
+  .on("end", async () => {
     console.log(count);
     console.log(results[count-1][1]);
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
     while(count--){
-      (async () => {
-        console.log(results[2][0]);
-        const browser = await puppeteer.launch();
-        const page = await browser.newPage();
-        await page.goto('https://wp.pubg-glitch.com/2020/12/31/hello-world/');
-        await page.type('#comment', 'Comment'+ count)
-        await page.type('#author', results[count-1][1])
-        await page.type('#email', results[count-1][3])
-        await page.click('#submit')
+        console.log(count, results[count][1]);
+        try {
+          await page.goto('https://wp.pubg-glitch.com/2020/12/31/hello-world/');
+          await page.type('#comment', 'Comment'+ count)
+          await page.type('#author', results[count][1])
+          await page.type('#email', results[count][3])
+          await page.click('#submit')
+        } catch (error) {
+          console.log(error)
+        }
         console.log('Submitted!');
         // await browser.close();
-      })();
-    }
-        
+      };
   });
